c885913080dbecb6fc4531874563d056f5474b44
DeeAnnEye <danielashulamithjustin@gmail.com>
2022-02-22 20:13:55 +0530

    Add function

--- test.c
+++ test.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//header function
-void header(){
+FILE *fp;
+// header function
+void header()
+{
     printf("\n\t\t\t\033[1m        WELCOME TO CASHFLOW \033[0m\n");
     printf("\t\t\t   -----------------------------\t\t\n");
+}
+void addmoney()
+{
+    fp = fopen("./datafiles/cash.txt", "w");
+    fputs("Hello world", fp);
+    fclose(fp);
 }
 
 int main()
@@ -12,27 +20,24 @@ int main()
     // Header
     header();
     int n;
-    FILE *fp;
     printf("Choose an option:\n\n");
     printf("1.Add Money\n");
     printf("2.Add Expense\n");
     printf("3.View Cashflow\n");
-    scanf("%d",&n);
-    switch(n)
+    scanf("%d", &n);
+    switch (n)
     {
-        case 1:
+    case 1:
         printf("Add money");
-        fp  = fopen ("./datafiles/cash.txt", "w");
-        fputs("Hello world", fp);
-        fclose(fp);
+        addmoney();
         break;
-        case 2:
+    case 2:
         printf("Add Expense");
         break;
-        case 3:
+    case 3:
         printf("View Cashflow");
         break;
-        default:
+    default:
         printf("Invalid option");
         break;
     }
