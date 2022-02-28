a2a1743af81e158be37412f52b3389173db128cd
DeeAnnEye <danielashulamithjustin@gmail.com>
2022-02-28 16:20:41 +0530

    Add structure to file

--- test.c
+++ test.c
@@ -3,12 +3,13 @@
 #include <string.h>
 
 FILE *fp;
+FILE *fr;
 
 struct Cash
 {
-   int Income;
-   int Expense;
-   char Description[50];
+    int Income;
+    int Expense;
+    char Description[50];
 };
 
 // header function
@@ -17,29 +18,38 @@ void header()
     printf("\n\t\t\t\033[1m        WELCOME TO CASHFLOW \033[0m\n");
     printf("\t\t\t   -----------------------------\t\t\n");
 }
-void addmoney(struct Cash c)
+
+void mainmenu()
 {
-    char desc[20];
-    fp = fopen("./datafiles/cash.txt", "a");
-    if (fp == NULL)
-        {
-            fprintf(stderr, "\nError opened file\n");
-            exit (1);
-        }
-    c.Income = 1334;
-    c.Expense = 0;
-    strcpy(c.Description, "wdl"); 
-    // c.Description = desc;
-    fwrite (&c, sizeof(struct Cash), 1, fp);
-    if(fwrite != 0)
-            printf("Entry Added\n");
-        else
-            printf("Error writing entry !\n");
-    while(fread(&c, sizeof(struct Cash), 1, fp))
-        printf ("income = %d expense = %d description = %s \n", c.Income,
-        c.Expense, c.Description);
- 
-    fclose(fp);
+    printf("Choose an option:\n\n");
+    printf("1.Add Money\n");
+    printf("2.Add Expense\n");
+    printf("3.View Cashflow\n");
+}
+void addmoney(struct Cash cash)
+{
+   printf("Enter Income:");
+   scanf("%d",&cash.Income);
+   printf("Enter Expense:");
+   scanf("%d",&cash.Expense);
+   printf("Enter Description:");
+   scanf("%s",&cash.Description);
+
+   fp= fopen ("datafiles/cash.txt", "a");
+   if (fp == NULL) {
+      fprintf(stderr, "\nError to open the file\n");
+      exit (1);
+   }
+    fwrite (&cash, sizeof(struct Cash), 1, fp);
+   if(fwrite != 0)
+      printf("Contents to file written successfully !\n");
+   else
+      printf("Error writing file !\n");
+   fclose (fp);
+//    fr= fopen ("datafiles/cash.txt", "r");
+//     while(fread(&cash, sizeof(struct Cash), 1, fr))
+//       printf (" income = %d expense = %d description = %s\n", cash.Income, cash.Expense, cash.Description);
+//    fclose (fr);
 }
 
 int main()
@@ -47,19 +57,14 @@ int main()
     // Header
     header();
     int n;
-
-    struct Cash c;
-
-    printf("Choose an option:\n\n");
-    printf("1.Add Money\n");
-    printf("2.Add Expense\n");
-    printf("3.View Cashflow\n");
+    struct Cash cash;
+    
+    mainmenu();
     scanf("%d", &n);
     switch (n)
     {
     case 1:
-        printf("Add money");
-        addmoney(c);
+        addmoney(cash);
         break;
     case 2:
         printf("Add Expense");
